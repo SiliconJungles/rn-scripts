5f8f36eed320adc61b4653f0fbf14ed594a7bfc1
Jaslyn <jaslyn@siliconjungles.com>
2020-01-02 18:23:13 +0800

    Parse properly

--- src/tools.ts
+++ src/tools.ts
@@ -4,7 +4,7 @@ import Shell from './lib/Shell'
 import Tools from './lib/Tools'
 
 export async function get_build_number(): Promise<void> {
-	let build_no = Tools.version_from_git('develop').toString()
+	let build_no = Tools.version_from_git('origin/develop').toString()
 	await Shell.sh_2(`echo ${build_no}`)
 }
 
